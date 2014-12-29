3b8bb84b7b33ca012393f09a9ca1a960c4bb0c86
Lucas Soumille <you@example.com>
2014-12-29 15:07:00 +0100

    catalan

--- src/math/MotBienParenthese.java
+++ src/math/MotBienParenthese.java
@@ -2,31 +2,49 @@ package math;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * @author PTung
  */
 public class MotBienParenthese {
-    public static final char MOT_VIDE = 'ε';
-    public static final String STR_VIDE = "ε";
-    public ArrayList<String> AllMotsBP;
 
     /**
      * Affiche tous les mots bien parenthésés de longueur 2n
      *
      * @param n
      */
-    public static ArrayList<String> enumMotsBP(int n) {
-        if ( n == 1 ){
-            return new ArrayList<String>(Arrays.asList("()"));
+    public static Set<String> enumMotsBP(int n) {
+        if (n == 1) {
+        return new HashSet<String>(Arrays.asList("()"));
         }
-        ArrayList<String> BPrecup = enumMotsBP(n-1);
-        ArrayList<String> newBP = new ArrayList<String>();
-        for(String str : BPrecup){
-            newBP.add("()" + str);
-            newBP.add(str + "()");
-            newBP.add("(" + str + ")");
+        Set<String> actuelMotsBP = enumMotsBP(n-1);
+        Set<String> nouvMotsBP = new HashSet<String>();
+        for (String mot : actuelMotsBP) {
+        nouvMotsBP.add("()"+ mot);
+        nouvMotsBP.add(mot +"()");
+        nouvMotsBP.add("("+ mot +")");
         }
-        return newBP;
+        return nouvMotsBP;
+    }
+    
+    /*
+     * Q2 - D'apres nos recherche sur internet, ici les C_n correspondent 
+     * aux nombres Catalans, défini par Cn = (2n)!/((n+1)! * n!). Afin de déterminer
+     * une formule de recurrence on détermine Cn+1 = (2n+2)!/((n+1)!(n+2)!)
+     * Ce qui nous donne le résultat suivant :
+     *      C_n+1 = (2(2n + 1)/(n+2)) * C_n  pour n >= 0
+     *       
+     */
+    
+    /*
+     * calcul le nombre catalan correspondant au nombre passé en paramètre
+     */
+    public static long catalan (int n){
+        if(n == 1) return 1;
+        n -= 1;
+        long calcul = (2*(2*n+1)/(n+2)) * catalan(n);
+        return calcul;
     }
 }
--- src/test/TestMotBienParenthese.java
+++ src/test/TestMotBienParenthese.java
@@ -1,6 +1,8 @@
 package test;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 import math.MotBienParenthese;
 
@@ -8,11 +10,29 @@ import math.MotBienParenthese;
  * @author PTung
  */
 public class TestMotBienParenthese {
-    public static void main(String[] args) {
+    
+    public static void testCatalan(int n){
+        long catalani;
+        long t0;
+        long temps;
+        for(int i=10;i<=n;i++){
+           t0 = System.nanoTime();//System.currentTimeMillis();
+           catalani=MotBienParenthese.catalan(i);
+           temps= System.nanoTime() - t0;//System.currentTimeMillis()-t0;
+           System.out.println("Temps de calcul du "+i+"ème nombre de Catalan : "+temps);
+        }
+    }//testCatalan
+        
+    public static void testMotsParenthese(){
         int n = 3;
-        ArrayList<String> allBP = MotBienParenthese.enumMotsBP(n); 
+        Set<String> allBP = MotBienParenthese.enumMotsBP(n); 
         for(String str : allBP){
             System.out.println(str);
         }
     }
+        
+    public static void main(String[] args) {
+        testCatalan(30);
+    }
+   
 }
